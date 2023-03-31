67a815a06fb8d295d900c34de89397eec953668c
mossa <martin.ossa.8@gmail.com>
2023-03-31 08:47:07 +0200

    Add home and categories components

---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+const Categories = () => {
+    return (
+        <div>
+            <h1>Categories</h1>
+                <button>Check status</button>
+        </div>
+    )
+}
+
+export default Categories;
