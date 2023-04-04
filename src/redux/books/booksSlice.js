bafbbbfe22bbff6df2956871c85f30bd108d2fad
mossa <martin.ossa.8@gmail.com>
2023-04-04 12:14:07 +0200

    Fix styling issues

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,19 +1,18 @@
-
 import { createSlice } from '@reduxjs/toolkit';
 
-// Includes an array of books (initial state: empty array)
 const booksSlice = createSlice({
+// create new book as empty array
   name: 'books',
   initialState: { books: [] },
   reducers: {
-// Includes a reducer that adds a book
+    // reducer that adds a book
     addBook: (state, action) => {
       state.books.push(action.payload);
     },
-// Includes a reducer that removes a book
-    removeBook: (state, action) => {
-      state.books = state.books.filter(book => book.id !== action.payload);
-    },
+    // reducer that removes a book
+    removeBook: (state, action) => ({
+      ...state, books: state.books.filter((book) => book.id !== action.payload),
+    }),
   },
 });
 
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,13 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 // Includes an array of categories (initial state: empty array)
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState: { categories: [] },
-// Includes a reducer that checks the status and always returns "Under construction" (the initial state should check to that string)
+  // Includes a reducer that checks the status and always returns "Under construction"
   reducers: {
-    checkStatus: (state) => {
-      return 'Under construction';
-    },
+    checkStatus: () => 'Under construction',
   },
 });
 
